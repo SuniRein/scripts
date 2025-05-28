1d5c2ca70a1880d9e0f09be7a1039790f1983c87
SuniRein <sunirein@qq.com>
2025-05-28 14:27:37 +0800

    feat(Bilinovel): adapt to linovelib.com

--- scripts/BilinovelAntiBlock.js
+++ scripts/BilinovelAntiBlock.js
@@ -2,10 +2,11 @@
 // @name         BilinovelAntiBlock
 // @name:zh      Bilinovel 反广告屏蔽
 // @namespace    https://github.com/SuniRein/scripts
-// @version      1.1.0
+// @version      1.2.0
 // @description  抑制 Bilinovel 检测到广告屏蔽插件后隐藏内容
 // @author       SuniRein
 // @match        https://www.bilinovel.com/*
+// @match        https://www.linovelib.com/*
 // @grant        none
 // @run-at       document-start
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilinovel.com
@@ -22,51 +23,86 @@
     const maxWaitTime = 15000;
     let timeWaited = 0;
 
-    let targetId = 'acontent';
-    let headerClass = 'atitle';
-
-    const currentUrl = window.location.href;
-    if (currentUrl.endsWith('catalog')) {
-        targetId = 'volumes';
-        headerClass = 'module-header';
+    // 根据不同网页定位目标元素
+    function analyzePage() {
+        const hostname = window.location.hostname;
+        const href = window.location.href;
+        if (hostname == 'www.bilinovel.com') {
+            console.log('Bilinovel: 检测到 Bilinovel 网站');
+            if (href.includes('/catalog')) {
+                return {
+                    getContent: () => document.getElementById('volumes'),
+                    isTitle: (ele) => ele.classList.contains('module-header'),
+                };
+            }
+            return {
+                getContent: () => document.getElementById('acontent'),
+                isTitle: (ele) => ele.classList.contains('atitle'),
+            };
+        } else if (hostname == 'www.linovelib.com') {
+            console.log('Bilinovel: 检测到 Linovelib 网站');
+            if (href.includes('/catalog')) {
+                return {
+                    getContent: () => document.getElementById('volume-list'),
+                    isTitle: (ele) => ele.classList.contains('section_title'),
+                };
+            }
+            return {
+                getContent: () => document.getElementById('TextContent'),
+                isTitle: (ele) => ele.tagName === 'H1',
+            };
+        }
     }
+
+    function showElement(element) {
+        element.style.setProperty('display', 'block', 'important');
+    }
+
+    function hideElement(element) {
+        element.style.setProperty('display', 'none', 'important');
+    }
+
+    const { getContent, isTitle } = analyzePage();
 
     console.log('Bilinovel: 开始轮询获取目标元素...');
 
-    const display_account = (element) => {
+    function displayContent(content) {
         try {
             // 显示文本
-            element.style.setProperty('display', 'block', 'important');
-            element.classList.remove('adv-box');
+            showElement(content);
+            content.classList.remove('adv-box');
             console.log('Bilinovel: 显示目标元素');
 
             // 移除广告框
-            if (element.previousElementSibling && !element.previousElementSibling.classList.contains(headerClass)) {
-                element.previousElementSibling.style.setProperty('display', 'none', 'important');
+            const adBox = content.previousElementSibling;
+            if (adBox && !isTitle(adBox)) {
+                hideElement(adBox);
                 console.log('Bilinovel: 隐藏广告框');
             }
         } catch (e) {
             console.error('Bilinovel: 修改目标元素时出错:', e);
         }
-    };
+    }
 
     const intervalId = setInterval(() => {
-        const element = document.getElementById(targetId);
+        const content = getContent();
         timeWaited += checkElementInterval;
 
-        if (element) {
+        if (content) {
             console.log('Bilinovel: 找到目标元素');
             clearInterval(intervalId);
+
+            displayContent(content);
 
             // 监测目标元素样式改变
             const observer = new MutationObserver((mutationsList) => {
                 for (const mutation of mutationsList) {
                     if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-                        display_account(element);
+                        displayContent(content);
                     }
                 }
             });
-            observer.observe(element, {
+            observer.observe(content, {
                 attributes: true,
                 attributeFilter: ['style'],
             });
@@ -77,7 +113,7 @@
                 const headerObserver = new MutationObserver((mutationsList) => {
                     for (const mutation of mutationsList) {
                         if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
-                            headerElement.style.setProperty('display', 'none', 'important');
+                            hideElement(headerElement);
                             console.log('Bilinovel: 隐藏广告横幅');
                         }
                     }
